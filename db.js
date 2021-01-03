22efc8e464c24d0ff78af9524f9a13e51a48f1c1
mertyjsx <orkunmertgs41@gmail.com>
2021-01-03 14:04:27 +0300

    s

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "hello123",
-  port: 5432,
-  database: "apartment"
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  port: process.env.port,
+  database: process.env.database
 });
 
 module.exports = pool;
